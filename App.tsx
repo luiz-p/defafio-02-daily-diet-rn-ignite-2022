ea18c944cc659fcfd29f2ed578de11db329a71bc
Luiz Pires <lpiresjunior@gmail.com>
2022-09-28 12:14:37 -0300

    feat: create theme provider and theme types

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   View
 } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
 
 import {
   NunitoSans_400Regular,
@@ -12,18 +13,20 @@ import {
   useFonts
 } from '@expo-google-fonts/nunito-sans'
 
+import theme from './src/theme'
+
 export default function App () {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       {fontsLoaded
         ? <View style={styles.container}>
           <Text>Open up App.tsx to start working on your app!</Text>
           <StatusBar style="auto" />
         </View>
         : <ActivityIndicator />}
-    </>
+    </ThemeProvider>
 
   )
 }
