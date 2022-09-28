30b218e3459623295c802ab62fde091ac130617b
Luiz Pires <lpiresjunior@gmail.com>
2022-09-28 12:15:22 -0300

    feat: create app routes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,4 @@
-import { StatusBar } from 'expo-status-bar'
-import {
-  ActivityIndicator,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
+import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 
 import {
@@ -13,6 +7,7 @@ import {
   useFonts
 } from '@expo-google-fonts/nunito-sans'
 
+import { Routes } from './src/routes'
 import theme from './src/theme'
 
 export default function App () {
@@ -20,22 +15,8 @@ export default function App () {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded
-        ? <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-        : <ActivityIndicator />}
+      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
     </ThemeProvider>
 
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
