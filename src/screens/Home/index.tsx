f7aaee305163ae2ce4810e60202a2443d8ce5968
Luiz Pires <lpiresjunior@gmail.com>
2022-09-29 13:03:05 -0300

    feat: create stats button on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
 import logoImg from '@assets/logo.png'
+import { Highlight } from '@components/Highlight'
 
 import * as S from './styles'
 
@@ -9,6 +10,11 @@ export function Home () {
         <S.Logo source={logoImg} />
         <S.UserImg source={{ uri: 'https://github.com/luiz-p.png' }} />
       </S.Header>
+
+      <S.StatsButton>
+        <S.ArrowIcon />
+        <Highlight title='90,86%' subtitle='das refeições dentro da dieta' />
+      </S.StatsButton>
     </S.Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { ArrowUpRight } from 'phosphor-react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
@@ -22,4 +23,20 @@ export const UserImg = styled.Image`
   height: 40px;
   border: 2px solid ${({ theme }) => theme.COLORS.GRAY_2};
   border-radius: 20px;
+`
+
+export const StatsButton = styled.TouchableOpacity`
+  padding: 20px 8px;
+  margin-top: 32px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.COLORS.GREEN_LIGHT};
+`
+
+export const ArrowIcon = styled(ArrowUpRight).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.COLORS.GREEN_DARK
+}))`
+  position: absolute;
+  right: 0;
+  margin: 8px;
 `
