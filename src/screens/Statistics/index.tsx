89c5092d50f7e24558794233029b429deaf9019b
Luiz Pires <lpiresjunior@gmail.com>
2022-09-30 21:33:37 -0300

    feat: add statistics screen content

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Highlight } from '@components/Highlight'
+import { useNavigation } from '@react-navigation/native'
 
 import * as S from './styles'
 
 export function Statistics () {
+  const navigation = useNavigation()
+
+  const [isHighPercent, setIsHighPercent] = useState(true)
+
   return (
     <S.Container>
-      <S.Title>Statistics</S.Title>
+      <S.Header isHighPercent={isHighPercent}>
+        <S.BackButton onPress={() => navigation.navigate('home')}>
+          <S.ArrowIcon isHighPercent={isHighPercent} />
+        </S.BackButton>
+        <Highlight title="90,86%" subtitle="das refeições dentro da dieta" />
+      </S.Header>
+
+      <S.StatisticsWrapper>
+        <S.Title>Estatísticas gerais</S.Title>
+
+        <S.Card>
+          <Highlight
+            title="4"
+            subtitle="melhor sequência de pratos dentro da dieta"
+            type="SECONDARY"
+          />
+        </S.Card>
+
+        <S.Card>
+          <Highlight
+            title="109"
+            subtitle="refeições registradas"
+            type="SECONDARY"
+          />
+        </S.Card>
+
+        <S.CardWrapper>
+          <S.Card cardType="HIGH_PERCENT">
+            <Highlight
+              title="32"
+              subtitle="refeições dentro da dieta"
+              type="SECONDARY"
+            />
+          </S.Card>
+
+          <S.Separator />
+
+          <S.Card cardType="LOW_PERCENT">
+            <Highlight
+              title="77"
+              subtitle="refeições dentro da dieta"
+              type="SECONDARY"
+            />
+          </S.Card>
+        </S.CardWrapper>
+      </S.StatisticsWrapper>
     </S.Container>
   )
 }
--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,5 +1,81 @@
-import styled from 'styled-components/native'
+import { ArrowLeft } from 'phosphor-react-native'
+import { Dimensions, TouchableOpacityProps } from 'react-native'
+import styled, { css } from 'styled-components/native'
 
-export const Container = styled.View``
+const { width } = Dimensions.get('window')
 
-export const Title = styled.Text``
+export type StatisticsStyleProps = {
+  isHighPercent?: boolean;
+  cardType?: 'HIGH_PERCENT' | 'LOW_PERCENT';
+};
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Header = styled.View<StatisticsStyleProps>`
+  height: 200px;
+  justify-content: center;
+  background-color: ${({ theme, isHighPercent }) =>
+    isHighPercent ? theme.COLORS.GREEN_LIGHT : theme.COLORS.RED_LIGHT};
+`
+
+export const BackButton = styled.TouchableOpacity.attrs<TouchableOpacityProps>(
+  () => ({
+    hitSlop: { top: 30, left: 30, bottom: 30, right: 30 }
+  })
+)<TouchableOpacityProps>`
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 56px 24px;
+`
+
+export const ArrowIcon = styled(ArrowLeft).attrs<StatisticsStyleProps>(
+  ({ theme, isHighPercent }) => ({
+    size: 24,
+    color: isHighPercent ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK
+  })
+)<StatisticsStyleProps>``
+
+export const StatisticsWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  margin-top: -33px;
+  padding: 33px 24px 0 24px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_7};
+  border-radius: 20px;
+`
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_1};
+    margin-bottom: 23px;
+  `}
+`
+
+export const Card = styled.View<StatisticsStyleProps>`
+  width: 100%;
+  padding: 16px;
+  margin-bottom: 12px;
+  border-radius: 8px;
+  background-color: ${({ theme, cardType }) =>
+    cardType === 'HIGH_PERCENT'
+      ? theme.COLORS.GREEN_LIGHT
+      : cardType === 'LOW_PERCENT'
+      ? theme.COLORS.RED_LIGHT
+      : theme.COLORS.GRAY_6};
+`
+
+export const CardWrapper = styled.View`
+  width: ${(width - 48) / 2 - 6}px;
+  flex-direction: row;
+  align-self: flex-start;
+`
+
+export const Separator = styled.View`
+  width: 12px;
+`
