d0b6f1bb53599b4d3537356c651d37a247c93e27
Luiz Pires <lpiresjunior@gmail.com>
2022-10-03 13:40:02 -0300

    feat: add inputs to new meal screen

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
+import { DefaultHeader } from '@components/DefaultHeader'
+import { Input } from '@components/Input'
+
 import * as S from './styles'
 
 export function NewMeal () {
   return (
     <S.Container>
-      <S.Title>NewMeal</S.Title>
+      <DefaultHeader title="Nova Refeição" />
+
+      <S.Form>
+        <S.Label>Nome</S.Label>
+        <Input />
+
+        <S.Label>Descrição</S.Label>
+        <Input isTextArea />
+
+        <S.TwoColumnsWrapper>
+          <S.PickerWrapper>
+            <S.Label>Data</S.Label>
+          </S.PickerWrapper>
+
+          <S.PickerWrapper>
+            <S.Label>Hora</S.Label>
+          </S.PickerWrapper>
+        </S.TwoColumnsWrapper>
+
+        <S.Label>Está dentro da dieta?</S.Label>
+      </S.Form>
     </S.Container>
   )
 }
--- src/screens/NewMeal/styles.ts
+++ src/screens/NewMeal/styles.ts
@@ -1,5 +1,33 @@
-import styled from 'styled-components/native'
+import { Dimensions } from 'react-native'
+import styled, { css } from 'styled-components/native'
 
-export const Container = styled.View``
+const { width } = Dimensions.get('window')
 
-export const Title = styled.Text``
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Form = styled.View`
+  flex: 1;
+  margin-top: -28px;
+  border-radius: 20px;
+  padding: 40px 24px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_7};
+`
+
+export const Label = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_2};
+  `}
+`
+
+export const TwoColumnsWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const PickerWrapper = styled.View`
+  width: ${(width - 48) / 2 - 10}px;
+`
