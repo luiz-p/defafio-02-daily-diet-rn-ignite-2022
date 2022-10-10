a5030aca3e893952f09d693fd7299c62865d5c56
Luiz Pires <lpiresjunior@gmail.com>
2022-10-10 11:20:41 -0300

    refactor: remove console.log

--- src/storage/meal/updateMeal.ts
+++ src/storage/meal/updateMeal.ts
@@ -13,7 +13,6 @@ export async function updateMeal (mealToUpdate: MealStorageDTO) {
       const date = await findByDate(storedMeals, mealToUpdate)
       const mealToEdit = date.data.findIndex(meal => meal.id === mealToUpdate.data[0].id)
       date.data[mealToEdit] = mealToUpdate.data[0]
-      console.log('new meal:: ', storedMeals)
 
       await AsyncStorage.setItem(MEALS_COLLECTION, JSON.stringify(storedMeals))
     }
